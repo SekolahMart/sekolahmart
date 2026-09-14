bf5d54eda09d951aafb80b5184ed771f2dbaf344
SekolahMart <kiyaeaji@gmail.com>
2026-09-14 15:59:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://kpzyyhfeusnvumglczlk.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_JBbo32Cx_8kS_ybQ6g-q8A_oG0mMW7m";
+const SUPABASE_ANON_KEY = "sb_publishable_JBbO32Cx_8kS_ybQ6g-q8A_oG0mMW7m";
 
 const sb = window.supabase.createClient(
   SUPABASE_URL,

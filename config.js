681281913a1a2c9b67e5a556ac000d0e82afa69e
SekolahMart <kiyaeaji@gmail.com>
2026-09-14 13:49:24 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://kpzyyhfeushvumglczlk.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_JBbo32Cx_8kS_ybQ6g-q8A_oG0mMW7m";
+
+const sb = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
